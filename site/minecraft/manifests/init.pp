60424f61c83cde345cca0af49eb4f74bd90266cc
pandey-netizen <70861438+pandey-netizen@users.noreply.github.com>
2020-09-06 23:27:47 -0700

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,6 +1,7 @@
-class minecraft {
+class minecraft (
   $url = 'https://launcher.mojang.com/v1/objects/c5f6fb23c3876461d46ec380421e42b289789530/server.jar'
   $install_dir = '/opt/minecraft'
+){
   file {$install_dir:
     ensure => directory,
   }
