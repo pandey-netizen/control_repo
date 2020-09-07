0cef05376986185cb5df1ceaff3f38a924adb6ee
pandey-netizen <70861438+pandey-netizen@users.noreply.github.com>
2020-09-06 22:38:57 -0700

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -1,23 +1,23 @@
 class minecraft {
-  file {'/opt/minecraft':
+  file {‘/opt/minecraft’:
     ensure => directory,
   }
-  file {'/opt/minecraft/minecraft_servr.jar':
+  file {‘/opt/minecraft/minecraft_server.jar’:
     ensure => file,
     source => 'https://launcher.mojang.com/v1/objects/c5f6fb23c3876461d46ec380421e42b289789530/server.jar',
   }
-  package {'java':
+  package {‘java’:
     ensure => present,
   }
-  file {'/opt/minecraft/eula.txt':
-    ensure => file,
-    content => 'eula=true',
+  file {‘/opt/minecraft/eula.txt’:
+    ensure  => file,
+    content => ‘eula=true’
   }
-  file {'/etc/systemd/system/minecraft.service':
+  file {‘/etc/systemd/system/minecraft.service’:
     ensure => file,
-    source => 'puppet:///modules/minecraft/minecraft.service',
-  }  
-  service {'minecraft':
+    source => ‘puppet:///modules/minecraft/minecraft.service’,
+  }
+  service { ‘minecraft’:
     ensure => running,
     enable => true,
   }
